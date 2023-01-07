f47b0f3bc967f9a53ad57669dca19d6cd906e15f
Ronald Martins <86543618+RonaldAlexMartins@users.noreply.github.com>
2023-01-07 00:35:58 -0300

    criando funçaõ de atualização das estatisticas

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 const controle = document.querySelector("[data-controle]")
+const estatisticas = document.querySelectorAll("[data-estatica]")
 const pecas = {
   "bracos": {
       "forca": 29,
@@ -35,6 +36,7 @@ const pecas = {
 controle.forEach((elemento) => { 
     elemento.addEventListener("click", () => {
         manipulaDados(evento.target.dataset.controle, evento.target.parentNode)
+        atualizaEstatisticas(evento.target.dataset.peca)
     })
 })
 
@@ -46,4 +48,11 @@ function manipulaDados(operacao, controle) {
     } else {
         peca.value = parseInt(peca.value) + 1
     }
+}
+
+function atualizaEstatisticas(peca) {
+  estatisticas.forEach( (elemento) => {
+    elemento.textContent = parseInt(elemento.textContent) + pecas[peca][elemento.dataset.estatisticas]
+  })
+
 }
